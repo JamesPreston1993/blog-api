f631bb01ddd414aa1ee8040311c756d67045ae2a
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-16 14:13:29 +0100

    Added blog controller

---
+++ src/blog.controller.js
@@ -0,0 +1,29 @@
+var Blog = require('./blog.model');
+
+module.exports = {
+    create: createBlog,
+    update: updateBlog,
+    delete: deleteBlog,
+    view: viewBlog,
+    viewMany: viewBlogs
+};
+
+function createBlog (req, res) {
+    
+}
+
+function updateBlog (req, res) {
+    
+}
+
+function deleteBlog (req, res) {
+    
+}
+
+function viewBlog (req, res) {
+    
+}
+
+function viewBlogs (req, res) {
+    
+}
