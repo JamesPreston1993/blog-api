9a1442a7ad257de84011efd465331c20ea841ef3
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-18 19:06:09 +0100

    Add not empty validator for blog model

--- src/blog.model.js
+++ src/blog.model.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var notEmptyValidator = [
+    function (val) {
+        return val.trim().length > 0;
+    }, 'Cannot be empty.'
+];
+
 var BlogSchema = new Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    creator: { type: String, required: true }, 
+    title: { type: String, required: true, validate: notEmptyValidator },
+    content: { type: String, required: true, validate: notEmptyValidator },
+    creator: { type: String, required: true, validate: notEmptyValidator }, 
     createdOn: { type: Date, default: Date.now }
 });
 
