e92d83cfba5b1f095e1e74a36b6e3e27ca4a7a2c
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-18 20:26:16 +0100

    Add unit tests for properties

--- test/blog.model.test.js
+++ test/blog.model.test.js
@@ -2,6 +2,34 @@ var Blog = require('./../src/blog.model');
 var assert = require('chai').assert;
 
 describe('blog model', function () {
+    describe('properties', function () {
+        describe('createdOn', function () {
+            it('populated with current date by default',function () {
+                var currentDate = new Date();
+
+                var blog = new Blog({
+                    title: 'My Blog',
+                    content: 'My blog contents',
+                    creator: 'username1'
+                });
+
+                assert.isAbove(blog.createdOn.getTime(),
+                    currentDate.getTime()
+                );
+            });
+        });
+        describe('lastUpdatedOn', function () {
+            it('not populated by default',function () {
+                var blog = new Blog({
+                    title: 'My Blog',
+                    content: 'My blog contents',
+                    creator: 'username1'
+                });
+
+                assert.typeOf(blog.lastUpdatedOn, 'undefined');
+            });
+        });
+    })
     describe('validators', function (){
         it('no error when all properties are valid', function (){
             var blog = new Blog({
