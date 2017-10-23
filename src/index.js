8f0e945731a75011f4560c7af57d47889af5e1a6
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-23 19:54:03 +0100

    Export blog functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 var blogModel = require('./blog.model');
 var blogController = require('./blog.controller');
+var blogFunctions = require('./blog.functions');
 var comment = require('./comment.model');
 var commentController = require('./comment.controller');
 
 module.exports = {
     blog: {
         model: blogModel,
-        controller: blogController
+        controller: blogController,
+        functions: blogFunctions
     },
     comment: {
         model: comment.model,
